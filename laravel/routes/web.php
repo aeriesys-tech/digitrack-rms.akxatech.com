0de06b5810d6ab6762fa286978c9a316f82368d1
Bharatesh Shanawad <bharatesh.shanawad-v@adityabirla.com-at-718927010107>
2025-01-31 08:03:18 +0530

    update

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+use Illuminate\Support\Facades\Mail;
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/send-test-email', function () {
+    Mail::raw('This is a test email from RMS.', function ($message) {
+        $message->to('bharatesh.s@akxatech.com','raghuraj.rao@akxatech.com','alokmoy.bose@akxatech.com')->subject('Test Email from RMS');
+    });
+
+    return 'Test email sent!';
 });
